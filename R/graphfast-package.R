44991b10b611353273dcadfb398df6b283466768
Kyle Haynes <5267027+KyleHaynes@users.noreply.github.com>
2025-10-06 13:36:48 +1000

    fxing ns

---
+++ R/graphfast-package.R
@@ -0,0 +1,9 @@
+#' @keywords internal
+#' @useDynLib graphfast, .registration=TRUE
+#' @importFrom Rcpp evalCpp
+#' @importFrom methods new
+"_PACKAGE"
+
+## usethis namespace: start
+## usethis namespace: end
+NULL
